2fd8abab6c67b36d3103f7e8590827dd7ab49fb9
Adhocmaster <adhocmaster@live.com>
2023-10-19 11:11:54 -0700

    graph of thought for categories and keywords

--- src/PromptCategory.ts
+++ src/PromptCategory.ts
@@ -5,26 +5,29 @@ const promptUtils = new PromptUtils(true);
 const dataUtils = new DataUtils();
 
 (async function() {
-    const txtData = (await dataUtils.read('data/text/amazon_2023_raw_whole.txt_anonymized'))._unsafeUnwrap()
-    // const txtData = (await dataUtils.read('data/text/amazon_empty_full'))._unsafeUnwrap()
-    console.log(txtData)
+    // const txtData = (await dataUtils.read('data/text/amazon_2023_raw_whole.txt_anonymized'))._unsafeUnwrap()
+    // // const txtData = (await dataUtils.read('data/text/amazon_empty_full'))._unsafeUnwrap()
+    // console.log(txtData)
+    const txtData = `Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil With Biotin & Essential Oils, Nourishing Treatment for Split Ends and Dry Scalp for All Hair Types, 2-Fluid Ounces
+    Bonnlo 3 Burner Outdoor Portable Propane Stove Gas Cooker, Heavy Duty Iron Cast Patio Burner with Detachable Stand Legs for Camp Cooking (3-Burner 225,000-BTU)
+    Genie 7155-TKV Smart Garage Door Opener StealthDrive Connect - Ultra Quiet opener, WiFi, Battery Backup - Works with Alexa & Google Home
+    VANMASS Universal Car Phone Mount,【Patent & Safety Certs】 Upgraded Handsfree Stand, Phone Holder for Car Dashboard Windshield Vent, Compatible with iPhone 14 13 12 Samsung Android & Pickup Truck
+    `;
+
     
     const nTokens = (await promptUtils.getNumberOfTokensForChat(txtData))._unsafeUnwrap()
     console.log("Number of tokens", nTokens)
 
     // const prompt = `You are an expert in understanding e-commerce. Can you get the product names from the following text? I also need the product brand, price, classification, keywords, and date purchased.
     // Respond with the name, brand, price, classification, keywords, and date only. Please, use JSON structure for output. :\n\n${txtData}` - This is unreliable
-    const prompt = `You are an expert in understanding e-commerce. I need to extract purchase information. I need all the output for each purchase in this format:
+    const prompt = `You are an expert in understanding product categories and keywords. I need to extract categories and keywords of some products:
     \n\nJSON format: \n
         {
             name: string,
-            brand: string,
-            price: number,
-            classification: string,
+            category: string,
             keywords: string[],
-            date: string
         }
-        I need the purchase history from the following content. A purchase history must have a product name, price, and date of purchase. It can also have brand, classification, keywords which are optional. Classification denotes the category of the product and keywords describe the products using a few key words. The purchase date and price cannot be null. Do not include a purchase information in the output if the purchase date or price is missing.
+        Classification denotes the category of the product and keywords describe the products using a few keywords. For categories choose from ['Hair Care', 'Outdoor Cooking', 'Home Improvement', 'Electronics'] only. A product has one category and multiple keywords. Here is a list of products seperated by new lines. 
         Give response in a JSON array in the preceding format. :\n\n${txtData}`
     // I need the purchase history from the following content. A purchase history must have a product name, price, and date of purchase. Can you get the product names from the following text? I also need the product brand, price, classification, keywords, and date purchased. 
     // Give response in a JSON array in the preceding format. :\n\n${txtData}`
