1c4c938e6f1e1e7671f065d7f6223dd3a41805a7
Adhocmaster <adhocmaster@live.com>
2023-10-19 11:50:10 -0700

    sub category improves categorization

--- src/PromptCategory.ts
+++ src/PromptCategory.ts
@@ -8,10 +8,52 @@ const dataUtils = new DataUtils();
     // const txtData = (await dataUtils.read('data/text/amazon_2023_raw_whole.txt_anonymized'))._unsafeUnwrap()
     // // const txtData = (await dataUtils.read('data/text/amazon_empty_full'))._unsafeUnwrap()
     // console.log(txtData)
-    const txtData = `Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil With Biotin & Essential Oils, Nourishing Treatment for Split Ends and Dry Scalp for All Hair Types, 2-Fluid Ounces
-    Bonnlo 3 Burner Outdoor Portable Propane Stove Gas Cooker, Heavy Duty Iron Cast Patio Burner with Detachable Stand Legs for Camp Cooking (3-Burner 225,000-BTU)
-    Genie 7155-TKV Smart Garage Door Opener StealthDrive Connect - Ultra Quiet opener, WiFi, Battery Backup - Works with Alexa & Google Home
-    VANMASS Universal Car Phone Mount,【Patent & Safety Certs】 Upgraded Handsfree Stand, Phone Holder for Car Dashboard Windshield Vent, Compatible with iPhone 14 13 12 Samsung Android & Pickup Truck
+    const txtData = `
+    [
+        {
+            product_id: 1,
+            product_name: "Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil With Biotin & Essential Oils, Nourishing Treatment for Split Ends and Dry Scalp for All Hair Types, 2-Fluid Ounces"
+        },
+        {
+            product_id: 2,
+            product_name: "Bonnlo 3 Burner Outdoor Portable Propane Stove Gas Cooker, Heavy Duty Iron Cast Patio Burner with Detachable Stand Legs for Camp Cooking (3-Burner 225,000-BTU)"
+        },
+        {
+            product_id: 3,
+            product_name: "Genie 7155-TKV Smart Garage Door Opener StealthDrive Connect - Ultra Quiet opener, WiFi, Battery Backup - Works with Alexa & Google Home"
+        },
+        {
+            product_id: 4,
+            product_name: "VANMASS Universal Car Phone Mount,【Patent & Safety Certs】 Upgraded Handsfree Stand, Phone Holder for Car Dashboard Windshield Vent, Compatible with iPhone 14 13 12 Samsung Android & Pickup Truck"
+        },
+        {
+            product_id: 5,
+            product_name: "Bonnlo 3 Burner Outdoor Portable Propane Stove Gas Cooker, Heavy Duty Iron Cast Patio Burner with Detachable Stand Legs for Camp Cooking (3-Burner 225,000-BTU)"
+        },
+        {
+            product_id: 6,
+            product_name: "Genie 7155-TKV Smart Garage Door Opener StealthDrive Connect - Ultra Quiet opener, WiFi, Battery Backup - Works with Alexa & Google Home"
+        },
+        {
+            product_id: 7,
+            product_name: "VANMASS Universal Car Phone Mount,【Patent & Safety Certs】 Upgraded Handsfree Stand, Phone Holder for Car Dashboard Windshield Vent, Compatible with iPhone 14 13 12 Samsung Android & Pickup Truck"
+        },
+        {
+            product_id: 9,
+            product_name: "Bonnlo 3 Burner Outdoor Portable Propane Stove Gas Cooker, Heavy Duty Iron Cast Patio Burner with Detachable Stand Legs for Camp Cooking (3-Burner 225,000-BTU)"
+        },
+        {
+            product_id: 10,
+            product_name: "Genie 7155-TKV Smart Garage Door Opener StealthDrive Connect - Ultra Quiet opener, WiFi, Battery Backup - Works with Alexa & Google Home"
+        },
+        {
+            product_id: 11,
+            product_name: "VANMASS Universal Car Phone Mount,【Patent & Safety Certs】 Upgraded Handsfree Stand, Phone Holder for Car Dashboard Windshield Vent, Compatible with iPhone 14 13 12 Samsung Android & Pickup Truck"
+        },
+
+    ]  
+    
+    
     `;
 
     
@@ -20,14 +62,15 @@ const dataUtils = new DataUtils();
 
     // const prompt = `You are an expert in understanding e-commerce. Can you get the product names from the following text? I also need the product brand, price, classification, keywords, and date purchased.
     // Respond with the name, brand, price, classification, keywords, and date only. Please, use JSON structure for output. :\n\n${txtData}` - This is unreliable
-    const prompt = `You are an expert in understanding product categories and keywords. I need to extract categories and keywords of some products:
+    const prompt = `You are an expert in understanding product categories and keywords. I need to extract categories and keywords of some products. Sub-category is more specific.  I need all the output in this format:
     \n\nJSON format: \n
         {
-            name: string,
+            product_id: number,
             category: string,
+            sub-category: string,
             keywords: string[],
         }
-        Classification denotes the category of the product and keywords describe the products using a few keywords. For categories choose from ['Hair Care', 'Outdoor Cooking', 'Home Improvement', 'Electronics'] only. A product has one category and multiple keywords. Here is a list of products seperated by new lines. 
+        Classification denotes the category of the product and keywords describe the products using a few keywords. For categories choose from ['Beauty & Health', 'Outdoors', 'Home & Gardening', 'Electronics', "Automotive"] only. A product has one category and multiple keywords. Here is a list of products seperated by new lines. 
         Give response in a JSON array in the preceding format. :\n\n${txtData}`
     // I need the purchase history from the following content. A purchase history must have a product name, price, and date of purchase. Can you get the product names from the following text? I also need the product brand, price, classification, keywords, and date purchased. 
     // Give response in a JSON array in the preceding format. :\n\n${txtData}`
